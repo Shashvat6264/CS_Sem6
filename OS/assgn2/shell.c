30c89adf1e1b547c5ee1dc7f9616389ccb33c8e4
Shashvat Gupta <shashvat6264@gmail.com>
2022-02-04 18:02:02 +0530

    Added to Shell Assignment

--- OS/assgn2/shell.c
+++ OS/assgn2/shell.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <sys/wait.h>
 #include <unistd.h>
+#include <signal.h>
 
 #define MAXCMDLEN 200
 #define MAXCMDNUM 50
@@ -25,18 +26,32 @@ void printDirName(){
     printf("\n%s", cwd);
 }
 
+void sigint_handler(int signum){
+	signal(SIGINT, sigint_handler);
+	printf("Got a Ctrl+C interrupt\n");
+	pid_t pid = getpid();
+	printf("Ending process with PID: %d", pid);
+	kill(pid, SIGKILL);
+	fflush(stdout);
+}
+
+void add_history(char *command){
+
+}
+
 int inputCmd(char* str){
 	//str = (char*) malloc(MAXCMDLEN * sizeof(char));
 	int n = scanf("%[^\n]", str);
+	add_history(str);
 	getchar();
 	//printf("%d %s\n", n,str);
 	return n;
 }
 
-char** parseCmd(char* cmd){
+char** parseCmd(char* cmd, char** words){
 	//printf("parsingg %s\n", str);
-	char *token, *str;
-	char** words= (char**)malloc(sizeof(char*)*MAXCMDNUM);
+	char* token, *str;
+	// char **words= (char **)malloc(sizeof(char*)*MAXCMDNUM);
 	str = strdup(cmd);
 	token = strtok(str, " ");
 	int i=0;
@@ -52,29 +67,84 @@ int parsePipes(char* cmd, char** cmdlist){
 
 }
 
-int ownCommandHandler(char** parsed){
+void openHelp()
+{
+    puts("\n***WELCOME TO MY SHELL HELP***"
+        "\nCopyright @ Suprotik Dey"
+        "\n-Use the shell at your own risk..."
+        "\nList of Commands supported:"
+        "\n>cd"
+        "\n>ls"
+        "\n>exit"
+        "\n>all other general commands available in UNIX shell"
+        "\n>pipe handling"
+        "\n>improper space handling");
+  
+    return;
+}
 
+int ownCommandHandler(char** parsed){
+	int NoOfOwnCmds = 4, i, switchOwnArg = 0;
+    char* ListOfOwnCmds[NoOfOwnCmds];
+    char* username;
+  
+    ListOfOwnCmds[0] = "exit";
+    ListOfOwnCmds[1] = "cd";
+    ListOfOwnCmds[2] = "help";
+    ListOfOwnCmds[3] = "hello";
+  
+    for (i = 0; i < NoOfOwnCmds; i++) {
+        if (strcmp(parsed[0], ListOfOwnCmds[i]) == 0) {
+            switchOwnArg = i + 1;
+            break;
+        }
+    }
+	
+    switch (switchOwnArg) {
+    case 1:
+        printf("\nGoodbye\n");
+        exit(0);
+    case 2:
+        chdir(parsed[1]);
+        return 1;
+    case 3:
+        openHelp();
+        return 1;
+    case 4:
+        username = getenv("USER");
+        printf("\nHello %s.\nMind that this is "
+            "not a place to play around."
+            "\nUse help to know more..\n",
+            username);
+        return 1;
+    default:
+        break;
+    }
+  
+    return 0;
 }
 
 
 int main()
 {
 	initShell();
+	signal(SIGINT, sigint_handler);
 
 	while(1){
 
 		printDirName();
 
 		char cmd[MAXCMDLEN];
-		char **parsedcmd;
+		char* parsedcmd[MAXCMDNUM];
 		printf("/myshell$ ");
 		
 		//if input is blank handle
 		if(!inputCmd(cmd)) continue;
 
+		parseCmd(cmd, parsedcmd);
 
-		parsedcmd = parseCmd(cmd);
-		//printf("##%s\n", cmd);
+		// if (ownCommandHandler(parseCmd)) continue;
+
 		pid_t pid;
 		pid = fork();
 		if(pid<0){
